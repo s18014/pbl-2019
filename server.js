7e60fb9262b08d34726eb298f655bdc098c56890
s18014 <s18014@std.it-college.ac.jp>
2019-05-22 16:00:18 +0000

    multer, body-parserでmultipart/form-dataに対応 テキストやファイルを同時に処理できるようになった multerでFileの取得 body-parserでjson形式で送られてきたテキストを処理

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
+const multer = require('multer')
+const bodyParser = require('body-parser')
+const path = require('path')
 const express = require('express')
+
 const app = express()
+const tmpDir = path.join(__dirname, 'tmp')
+const upload = multer({ dest: tmpDir })
 const portNo = 3000
 
 app.listen(portNo, () => {
   console.log(`server started on http://localhost:${portNo}`)
 })
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/', express.static('./public/'))
+
+app.post('/api/upload', upload.any(), (req, res) => {
+  console.log(req.files)
+  console.log(req.body)
+  res.json({ status: true })
+})
